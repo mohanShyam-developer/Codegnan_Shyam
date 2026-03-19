e408f6e8526d02361b96b3b3c814eaeca1b0254c
mohanShyam-developer <dammumohan828@gmail.com>
2026-03-19 20:08:38 +0530

    art expense calculator
    Signed-off-by: mohanShyam-developer <dammumohan828@gmail.com>

--- day3.py
+++ day3.py
@@ -1,6 +1,8 @@
+
 print("Welcome to Smart Expense Calculator".center(80))
 print("-" * 50)
 
+#Enter User details
 name = input("Enter your name: ")
 
 income = float(input("Enter your monthly income: "))
@@ -9,6 +11,7 @@ travel = float(input("Enter travel expenses: "))
 entertainment = float(input("Enter entertainment expenses: "))
 others = float(input("Enter other expenses: "))
 
+#Formula
 total_expense = food + travel + entertainment + others
 balance = income - total_expense
 
@@ -20,7 +23,7 @@ print("Remaining Balance:", balance)
 
 print("-" * 50)
 
-# Smart message (this is the impressive part 👇)
+#logic
 if balance > 0:
     print("Good job! You are saving money 👍")
 elif balance == 0:
