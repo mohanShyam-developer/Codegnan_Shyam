0967a4fc62a7e5487d0d5f2b7f1b307970c4bac0
mohanShyam-developer <dammumohan828@gmail.com>
2026-03-19 19:47:38 +0530

    smart expense calculator

---
+++ day3.py
@@ -0,0 +1,31 @@
+print("Welcome to Smart Expense Calculator".center(80))
+print("-" * 50)
+
+name = input("Enter your name: ")
+
+income = float(input("Enter your monthly income: "))
+food = float(input("Enter food expenses: "))
+travel = float(input("Enter travel expenses: "))
+entertainment = float(input("Enter entertainment expenses: "))
+others = float(input("Enter other expenses: "))
+
+total_expense = food + travel + entertainment + others
+balance = income - total_expense
+
+print("\n" + "-" * 50)
+print("Name:", name)
+print("Total Income:", income)
+print("Total Expenses:", total_expense)
+print("Remaining Balance:", balance)
+
+print("-" * 50)
+
+# Smart message (this is the impressive part 👇)
+if balance > 0:
+    print("Good job! You are saving money 👍")
+elif balance == 0:
+    print("You spent exactly your income ⚖️")
+else:
+    print("Warning! You are overspending ⚠️")
+
+print("-" * 50)
