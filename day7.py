4c748643075bdbdb22e79dd1db69c67979b11de7
mohanShyam-developer <dammumohan246@gmail.com>
2026-03-23 21:16:50 +0530

    Fix Git email to GitHub email

---
+++ day7.py
@@ -0,0 +1,55 @@
+
+
+##name=input("enter name:")
+##book_tickets="Dhurandhar"
+##ticket_cost=int(input("enter ticket amount:"))
+##
+####if 270 == ticket_cost:
+##    print(" ticket confirmed for Dhurander")
+##else :
+##    print("ticket confirmed for Ustaad Bhagat Singh")
+'''
+student_marks=int(input(" Enter marks :"))
+if student_marks>=90:
+     print("you got A+ GRADE")
+elif student_marks>=75:
+         print(" you got A grade")
+elif student_marks>=60:
+             print("you got B+ grade")
+else:
+                 print("fail")
+
+
+num_1=int(input("Enter number :"))        
+num_2=int(input("Enter number :"))
+'''
+a=input()
+print(eval(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+                
+    
