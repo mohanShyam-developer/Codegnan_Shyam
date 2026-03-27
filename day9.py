2bc70046b81fd9ad7c9548249af23e44f0ab39c6
mohanShyam-developer <dammumohan246@gmail.com>
2026-03-27 22:06:50 +0530

    student mark manager

--- day9.py
+++ day9.py
@@ -1,77 +1,49 @@
-##
-### Student Marks Manager
-##
-##marks = [78, 85, 90, 67, 88]
-##
-##print("Initial Marks List:")
-##print(marks)
-##
-### Add new mark
-##new_mark = int(input("Enter new mark to add: "))
-##marks.append(new_mark)
-##
-##print("After Adding Mark:")
-##print(marks)
-##
-### Insert mark at position
-##pos = int(input("Enter position to insert mark: "))
-##value = int(input("Enter mark to insert: "))
-##marks.insert(pos, value)
-##
-##print("After Inserting Mark:")
-##print(marks)
-##
-### Remove a mark
-##remove_mark = int(input("Enter mark to remove: "))
-##if remove_mark in marks:
-##    marks.remove(remove_mark)
-##
-##print("After Removing Mark:")
-##print(marks)
-##
-### Sort marks
-##marks.sort()
-##print("Sorted Marks:")
-##print(marks)
-##
-### Reverse marks
-##marks.reverse()
-##print("Reversed Marks:")
-##print(marks)
-##
-### Display highest and lowest marks
-##print("Highest Mark:", max(marks))
-##print("Lowest Mark:", min(marks))
-##
-##print("Total Marks Count:", len(marks))
 
-'''
-print(9+8)
-print("pyhton"+" "+"language")
+# Student Marks Manager
 
-concatenation
--------------
-this is nothing but ,a (+) behaviour..
-case01
----------
-intergers---this will act as addition for the int
+marks = [78, 85, 90, 67, 88]
 
-str=[1,2,3,4]
-print(str)
+print("Initial Marks List:")
+print(marks)
 
-tuple()--->
--------
-is collection of different datatype and this is represent by (),separated by (,)
-eg:
-thing =(1,"teja",[12,4],(6,7))'''
-thing=(12,89,"python",(23,"roy",[67,"python is a language",(7,8)],[8,("python",[34,9])]))
-print(thing[3][2][1][9])
+# Add new mark
+new_mark = int(input("Enter new mark to add: "))
+marks.append(new_mark)
 
-num=9
-num_2=90
-print(f"before swapping {num,num_2}")
-swap=num,num_2=num2,num
-print(result)
+print("After Adding Mark:")
+print(marks)
+
+# Insert mark at position
+pos = int(input("Enter position to insert mark: "))
+value = int(input("Enter mark to insert: "))
+marks.insert(pos, value)
+
+print("After Inserting Mark:")
+print(marks)
+
+# Remove a mark
+remove_mark = int(input("Enter mark to remove: "))
+if remove_mark in marks:
+    marks.remove(remove_mark)
+
+print("After Removing Mark:")
+print(marks)
+
+# Sort marks
+marks.sort()
+print("Sorted Marks:")
+print(marks)
+
+# Reverse marks
+marks.reverse()
+print("Reversed Marks:")
+print(marks)
+
+# Display highest and lowest marks
+print("Highest Mark:", max(marks))
+print("Lowest Mark:", min(marks))
+
+print("Total Marks Count:", len(marks))
 
 
 
@@ -103,6 +75,3 @@ print(result)
 
 
 
-
-
-print
