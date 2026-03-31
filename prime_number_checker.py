d8266d0ea244b76bf252da6df466109752c8561f
mohanShyam-developer <dammumohan246@gmail.com>
2026-03-31 22:56:46 +0530

    prime number

---
+++ prime_number_checker.py
@@ -0,0 +1,347 @@
+###To check whether it is  prime number or not .
+##user_input=int(input("enter a number"))#89
+##count=0                                #0
+##for j in range(1,user_input+1):     #(1,89+1)
+##    print(j)
+##    if user_input%j==0:
+##       count+=1
+##       print(count)
+##if count==2: 1057,197,9,86,17673
+##    print(f"{user_input} is prime number")
+##else:
+##    print(f"{user_input} is not prime number")
+##
+##num = int(input("Enter a number: "))
+##
+##if num > 1:
+##    for i in range(2, num):
+##        if num % i == 0:
+##            print(num, "is NOT a prime number")
+##            break
+##    else:
+##        print(num, "is a PRIME number")
+##else:
+##    print(num, "is NOT a prime number")
+
+
+##list_1=int([1057,197,9,86,17673])
+##any=[2,356,8,6,3,2,8]
+##empty_=[]
+##for j in any:
+##    if j not in empty_
+##    :
+##        empty_.append(j)
+##        print(empty_)
+
+# Python Day 14
+# Program 2: Prime Numbers Using List
+
+n = int(input("Enter the limit: "))
+
+prime_list = []
+
+for num in range(2, n + 1):
+
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        prime_list.append(num)
+
+print("Prime numbers up to", n, "are:")
+print(prime_list)
+
+
+
+
+###To check whether it is  prime number or not .
+##user_input=int(input("enter a number"))#89
+##count=0                                #0
+##for j in range(1,user_input+1):     #(1,89+1)
+##    print(j)
+##    if user_input%j==0:
+##       count+=1
+##       print(count)
+##if count==2: 1057,197,9,86,17673
+##    print(f"{user_input} is prime number")
+##else:
+##    print(f"{user_input} is not prime number")
+##
+##num = int(input("Enter a number: "))
+##
+##if num > 1:
+##    for i in range(2, num):
+##        if num % i == 0:
+##            print(num, "is NOT a prime number")
+##            break
+##    else:
+##        print(num, "is a PRIME number")
+##else:
+##    print(num, "is NOT a prime number")
+
+
+##list_1=int([1057,197,9,86,17673])
+##any=[2,356,8,6,3,2,8]
+##empty_=[]
+##for j in any:
+##    if j not in empty_
+##    :
+##        empty_.append(j)
+##        print(empty_)
+
+# Python Day 14
+# Program 2: Prime Numbers Using List
+
+n = int(input("Enter the limit: "))
+
+prime_list = []
+
+for num in range(2, n + 1):
+
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        prime_list.append(num)
+
+print("Prime numbers up to", n, "are:")
+print(prime_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###To check whether it is  prime number or not .
+##user_input=int(input("enter a number"))#89
+##count=0                                #0
+##for j in range(1,user_input+1):     #(1,89+1)
+##    print(j)
+##    if user_input%j==0:
+##       count+=1
+##       print(count)
+##if count==2: 1057,197,9,86,17673
+##    print(f"{user_input} is prime number")
+##else:
+##    print(f"{user_input} is not prime number")
+##
+##num = int(input("Enter a number: "))
+##
+##if num > 1:
+##    for i in range(2, num):
+##        if num % i == 0:
+##            print(num, "is NOT a prime number")
+##            break
+##    else:
+##        print(num, "is a PRIME number")
+##else:
+##    print(num, "is NOT a prime number")
+
+
+##list_1=int([1057,197,9,86,17673])
+##any=[2,356,8,6,3,2,8]
+##empty_=[]
+##for j in any:
+##    if j not in empty_
+##    :
+##        empty_.append(j)
+##        print(empty_)
+
+# Python Day 14
+# Program 2: Prime Numbers Using List
+
+n = int(input("Enter the limit: "))
+
+prime_list = []
+
+for num in range(2, n + 1):
+
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        prime_list.append(num)
+
+print("Prime numbers up to", n, "are:")
+print(prime_list)
+
+
+print("Prime numbers up to", n, "are:")
+print(prime_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
