8a6eb9603834b36f4e98bdfb68f38dce7d27f844
mohanShyam-developer <dammumohan246@gmail.com>
2026-04-02 22:55:03 +0530

    count even and odd no's

---
+++ day16.py
@@ -0,0 +1,40 @@
+# Day 16 Simple Program
+# Count Even and Odd Numbers
+
+even_count = 0
+odd_count = 0
+
+numbers = []
+
+count = 1
+max_numbers = 5
+
+while count <= max_numbers:
+    num = int(input("Enter a number: "))
+    # skip negative numbers
+    if num < 0:
+        print("Negative skipped")
+        continue
+    # stop if 0 entered
+    if num == 0:
+        print("Zero entered. Stopping.")
+        break
+    # placeholder
+    if num == 1:
+        pass
+    # check even or odd
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+    numbers.append(num)
+    count += 1
+else:
+    print("You entered all numbers")
+print("\nNumbers you entered:")
+
+for i in range(len(numbers)):
+    print("Number", i+1, ":", numbers[i])
+
+print("\nTotal Even numbers:", even_count)
+print("Total Odd numbers:", odd_count)
