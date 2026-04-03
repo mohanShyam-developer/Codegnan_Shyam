1931ac42fdf263d65a07ea560489e2f36442433c
mohanShyam-developer <dammumohan246@gmail.com>
2026-04-03 21:52:37 +0530

    Patterns

---
+++ day17.py
@@ -0,0 +1,103 @@
+# Right Triangle Pattern
+##
+##rows = int(input("Enter number of rows: "))
+##for i in range(1, rows + 1):# Outer loop
+##    for j in range(i):      # Inner loop
+##        print("*", end="")
+##    print()
+##
+### Reverse Triangle Pattern
+##
+##rows = int(input("Enter number of rows: "))
+##for i in range(rows, 0, -1):# Outer loop
+##    for j in range(i):      # Inner loop
+##        print("*", end="")
+##    print()
+##
+### Pyramid Pattern
+##
+##rows = int(input("Enter number of rows: "))
+##
+##for i in range(1, rows + 1):
+##    for j in range(rows - i):# Print spaces
+##        print(" ", end="")
+##    for k in range(2 * i - 1):# Print stars
+##        print("*", end="")
+##    print()
+##
+### Reverse Pyramid
+##
+##rows = int(input("Enter number of rows: "))
+##for i in range(rows, 0, -1):
+##    # Spaces
+##    for j in range(rows - i):
+##        print(" ", end="")
+##    # Stars
+##    for k in range(2 * i - 1):
+##        print("*", end="")
+##    print()
+##
+##
+### Number Triangle Pattern
+##
+##rows = int(input("Enter number of rows: "))
+##for i in range(1, rows + 1):
+##    for j in range(1, i + 1):
+##        print(j, end="")
+##    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
