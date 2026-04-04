9fd282640e72af569e8d3e33ca303b9aa0f9c3c3
mohanShyam-developer <dammumohan246@gmail.com>
2026-04-04 23:54:05 +0530

    Exploring Strings & Text Processing

--- day18.py
+++ day18.py
@@ -1,20 +1,21 @@
 # Count vowels in a string
 
-##text = input("Enter a word: ")
-##count = 0
-##for i in text:
-##    if i.lower() in "aeiou":
-##        count += 1
-##print("Number of vowels:", count)
+text = input("Enter a word: ")
+count = 0
+for i in text:
+    if i.lower() in "aeiou":
+        count += 1
+print("Number of vowels:", count)
 
 ## Reverse string without using reverse()
 
-##text = input("Enter a word: ")
-##reverse = ""
-##for i in text:
-##    reverse = i + reverse
-##print("Reversed string:", reverse)
-##
+text = input("Enter a word: ")
+reverse = ""
+
+for i in text:
+    reverse = i + reverse
+print("Reversed string:", reverse)
+
 
 ## Check palindrome using for loop
 
